ef0b68224ca79580f669af69fe6066f08e2dbcd7
hasnain003 <mdhasnain839@gmail.com>
2018-02-05 20:54:23 +0530

    Add Different section like About,Home,Signin etc

---
+++ src/DbUnits.java
@@ -0,0 +1,21 @@
+
+import java.sql.ResultSet;
+import javax.swing.table.TableModel;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author User
+ */
+class DbUnits {
+
+    static TableModel resultSetTableModel(ResultSet rs) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ src/Javaconnect.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author User
+ */
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+public class Javaconnect {
+    Connection conn;
+    public static Connection ConnecrDb(){
+        try{
+            Class.forName("org.sqlite.JDBC");
+            Connection conn=DriverManager.getConnection("jdbc:sqlite:C:\\Users\\User\\Documents\\NetBeansProjects\\Library Management System1\\LibraryNew.sqlite");
+            return conn;
+        }catch(Exception e)
+        {
+            JOptionPane.showMessageDialog(null,e);
+            return null;
+        }
+    }
+}
